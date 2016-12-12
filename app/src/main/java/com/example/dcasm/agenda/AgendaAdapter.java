20bb398eee60c364c2aef1ceea66fe8d2ba4beba
dcasmor <dcasmor@outlook.com>
2016-12-13 00:13:26 +0100

    Stuff

--- app/src/main/java/com/example/dcasm/agenda/AgendaAdapter.java
+++ app/src/main/java/com/example/dcasm/agenda/AgendaAdapter.java
@@ -1,11 +1,30 @@
 package com.example.dcasm.agenda;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import java.util.List;
 
 /**
  * Created by dcasm on 12/12/2016.
  */
 
 public class AgendaAdapter extends ArrayAdapter<Contacto> {
+
+    public AgendaAdapter (Context context, List<Contacto> objects) {
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (null == convertView) {
+            convertView = inflater.inflate(R.layout. , parent, false);
+        }
+    }
 }
