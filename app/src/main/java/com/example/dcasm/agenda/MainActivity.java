cd0d513be0dad0cdca3ad0b95ec8bc092610ddbc
dcasmor <dcasmor@outlook.com>
2016-12-15 22:58:32 +0100

    Stuff

--- app/src/main/java/com/example/dcasm/agenda/MainActivity.java
+++ app/src/main/java/com/example/dcasm/agenda/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.dcasm.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
             fragmento = Fragmento.newInstance();
             getSupportFragmentManager().beginTransaction().add(R.id.content_main, fragmento).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent i = new Intent(this, AnadirContacto.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
