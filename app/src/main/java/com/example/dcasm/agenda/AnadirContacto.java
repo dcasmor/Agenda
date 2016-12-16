1bc708906e55db316cba966e29134b49cdfdbe2d
dcasmor <dcasmor@outlook.com>
2016-12-16 02:40:17 +0100

    Stuff

--- app/src/main/java/com/example/dcasm/agenda/AnadirContacto.java
+++ app/src/main/java/com/example/dcasm/agenda/AnadirContacto.java
@@ -1,10 +1,7 @@
 package com.example.dcasm.agenda;
 
 import android.app.Dialog;
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
